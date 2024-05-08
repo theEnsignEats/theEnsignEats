d59b7d143effeeb356f6fb19bd52ab467071f6a9
Pedro Perillo <pedrofperillo@gmail.com>
2024-05-08 14:39:28 -0600

    layout created

--- the-ensign-eats/src/app/layout.js
+++ the-ensign-eats/src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import header from "@/app/components/header";
+import footer from "@/app/components/footer"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +13,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <header />
+        <body className={inter.className}>{children}</body>
+      <footer />
+    </>
   );
 }
