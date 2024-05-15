5b321f8ff3ea2ddbfa92368da8cdb3dc20f70d86
antoniomartinez <mar20126@byui.edu>
2024-05-15 14:17:50 -0600

    Started the SeeOurMenu component, and added some images for the slideshow

---
+++ the-ensign-eats/src/app/components/SeeOurMenu/SeeOurMenu.jsx
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+import "./SeeOurMenu.css";
+
+const SeeOurMenu = () => {
+  return (
+    <div className="see-our-menu-container">
+      <div className="slideshow-container">
+        <div className="text-container">
+          <h1 className="montserrat">
+            A lot of options, at the tip of your fingers
+          </h1>
+          <p className="libre-baskerville-regular">
+            Get ready to tantalize your taste buds! Explore our mouthwatering
+            menu filled with delicious burger creations crafted just for you.
+            Click below to embark on a flavorful journey!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SeeOurMenu;
--- the-ensign-eats/src/app/page.jsx
+++ the-ensign-eats/src/app/page.jsx
@@ -2,17 +2,15 @@ import "tailwindcss/tailwind.css";
 import React from "react";
 import Header from "../app/components/header";
 import Footer from "../app/components/footer";
-
+import SeeOurMenu from "./components/SeeOurMenu/SeeOurMenu";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex justify-center items-center dark:bg-black dark:text-white">
       <div className="max-w-screen-lg py-8 md:py-12">
         <Header />
-      
-
+        <SeeOurMenu />
         <Footer />
-       
       </div>
     </div>
   );
