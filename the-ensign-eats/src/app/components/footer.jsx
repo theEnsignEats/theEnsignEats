a4832f2fb271fb742d2cec2583fa4034223beee6
Angela Cieslak <161291402+cieslak96@users.noreply.github.com>
2024-05-22 15:47:21 +0000

    footer without hoover

--- the-ensign-eats/src/app/components/footer.jsx
+++ the-ensign-eats/src/app/components/footer.jsx
@@ -39,24 +39,24 @@ const Footer = ({ links }) => {
 
       <div className="flex flex-col items-center ">
         <div className="flex items-center">
-          <svg className="w-6 h-6 text-white hover:text-yellow-200" fill="currentColor" viewBox="5 5 65 65" xmlns="http://www.w3.org/2000/svg">
+          <svg className="w-6 h-6 text-white " fill="currentColor" viewBox="5 5 65 65" xmlns="http://www.w3.org/2000/svg">
             <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
             <g id="SVGRepo_iconCarrier">
               <path d="M 27.9999 51.9062 C 41.0546 51.9062 51.9063 41.0547 51.9063 28.0000 C 51.9063 14.9219 41.0312 4.0938 27.9765 4.0938 C 14.8983 4.0938 4.0937 14.9219 4.0937 28.0000 C 4.0937 41.0547 14.9218 51.9062 27.9999 51.9062 Z M 21.8827 33.8360 C 16.0702 28.0469 12.3671 20.6640 16.7499 16.2813 C 17.0077 16.0234 17.2890 15.7656 17.5468 15.5078 C 18.8827 14.2422 20.1718 14.3125 21.3202 15.9297 L 24.3671 20.2656 C 25.3983 21.7656 25.1405 22.6094 24.0390 23.7813 L 23.0780 24.8360 C 22.7265 25.1640 22.8671 25.6094 23.0312 25.8906 C 23.4765 26.7344 24.7421 28.2344 26.1014 29.5938 C 27.5077 31.0000 28.9374 32.1953 29.8280 32.6875 C 30.1562 32.875 30.6249 32.9219 30.9296 32.6406 L 31.9374 31.6797 C 33.0624 30.5781 33.9765 30.2969 35.4296 31.3281 C 37.4452 32.7578 38.6640 33.6016 39.8593 34.4219 C 41.3358 35.5000 41.6874 36.8360 40.1874 38.1953 C 39.9296 38.4531 39.6952 38.7344 39.4374 38.9922 C 35.0546 43.3516 27.6952 39.6484 21.8827 33.8360 Z"></path>
             </g>
           </svg>
-          <span className="text-white ml-2 hover:text-yellow-200">801-384-8776</span>
+          <span className="text-white ml-2 ">801-384-8776</span>
         </div>
         <div className="flex items-center ">
-          <svg className="w-6 h-6 text-white hover:text-yellow-200" fill="currentColor" viewBox="2 2 25 25" xmlns="http://www.w3.org/2000/svg">
+          <svg className="w-6 h-6 text-white " fill="currentColor" viewBox="2 2 25 25" xmlns="http://www.w3.org/2000/svg">
             <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
             <g id="SVGRepo_iconCarrier">
               <path fillRule="evenodd" clipRule="evenodd" d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM7.005 9C7.005 8.45 7.45 8 8 8H16C16.55 8 17 8.45 17 9V15C17 15.55 16.55 16 16 16H8C7.45 16 7 15.55 7 15L7.005 9ZM12 12.5L8.00001 9.99997V15H16V9.99997L12 12.5ZM12 11.5L8.00001 9.00001H16L12 11.5Z"></path>
             </g>
           </svg>
-          <span className="text-white ml-2 hover:text-yellow-200">contact@ensigneats.com</span>
+          <span className="text-white ml-2 ">contact@ensigneats.com</span>
         </div>
       </div>
       
