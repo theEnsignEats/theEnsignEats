c8f0fba762ac246b5452ca2b66c0b8a298c4a4eb
Antonio <tonihmartinez@gmail.com>
2024-06-01 17:47:47 -0600

    Created menu cards

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,8 +5,38 @@ import Hero from "./components/hero";
 import Map from "./components/map";
 import CTA from "./components/CTA";
 import Cards from "@/app/components/cards";
+import Carousel from "./components/Carousel";
 
 export default function Home() {
+  const menuItems = [
+    {
+      image: 'carousel1.jpg',
+      name: 'Delicious Item 1',
+      description: 'A delightful item with fresh ingredients and rich flavors.',
+      price: '$8.99',
+    },
+    {
+      image: 'carousel2.jpg',
+      name: 'Delicious Item 2',
+      description: 'A second delightful item with fresh ingredients and rich flavors.',
+      price: '$9.99',
+    },
+    {
+      image: 'carousel3.webp',
+      name: 'Delicious Item 3',
+      description: 'A third delightful item with fresh ingredients and rich flavors.',
+      price: '$10.99',
+    },
+    {
+      image: 'carousel4.webp',
+      name: 'Delicious Item 4',
+      description: 'A fourth delightful item with fresh ingredients and rich flavors.',
+      price: '$11.99',
+    },
+    // Add more items as needed
+  ];
+
+
   return (
     <div className="min-h-screen flex justify-center items-center dark:bg-black dark:text-white">
       <div className="max-w-screen-lg py-8 md:py-12">
@@ -14,6 +44,7 @@ export default function Home() {
           <Hero />
           <Cards/>
           <CTA />
+          <Carousel items={menuItems} />
           <div>
             <Map />
           </div>
