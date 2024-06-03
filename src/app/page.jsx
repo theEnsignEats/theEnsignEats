30850939b4b87b6707ac8a1d0985738ba54daeeb
Pedro Perillo <pedrofperillo@gmail.com>
2024-06-03 16:14:53 -0600

    added carrousell and fixed home width and heith

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -39,12 +39,11 @@ export default function Home() {
 
   return (
     <div className="min-h-screen flex justify-center items-center dark:bg-black dark:text-white">
-      <div className="max-w-screen-lg py-8 md:py-12">
+      <div className="">
         <Layout>
           <Hero />
           <Cards/>
           <CTA />
-          <Carousel items={menuItems} />
           <div>
             <Map />
           </div>
