42998df5ca6798c045a04ee790cbd96250665bc2
antoniomartinez <mar20126@byui.edu>
2024-06-03 15:03:35 -0600

    Fixed Carousel conflict and added MenuCards to Menu page

--- src/app/components/ItemCard.jsx
+++ src/app/components/ItemCard.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const ItemCard = ({ image, name, description, price }) => {
   return (
     <div className="w-full h-full max-w-xs bg-white rounded-lg shadow-md overflow-hidden aspect-[3/4]">
       <img className="w-full h-3/5 object-cover" src={image} alt={name} />
       <div className="p-3">
-        <h2 className="text-black text-xl text-black font-bold mb-2">{name}</h2>
+        <h2 className="text-xl text-black font-bold mb-2">{name}</h2>
         <p className="text-gray-700 text-base mb-4">{description}</p>
-        <div className="text-black text-lg text-black font-semibold">{price}</div>
+        <div className="text-lg text-black font-semibold">{price}</div>
       </div>
     </div>
   );
