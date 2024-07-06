cb4a055a92f69389ff5058bddb78e4df518f3c45
Angela Cieslak <161291402+cieslak96@users.noreply.github.com>
2024-07-06 07:39:57 +0000

    Improved about page

--- src/__tests__/about.test.js
+++ src/__tests__/about.test.js
@@ -4,44 +4,44 @@ import '@testing-library/jest-dom';
 import AboutPage from '../app/about/page';
 
 describe('AboutPage', () => {
-    beforeEach(() => {
+  beforeEach(() => {
       render(<AboutPage />);
-    });
-  
-    test('renders Our History section', () => {
+  });
+
+  test('renders Our History section', () => {
       expect(screen.getByText('Our History')).toBeInTheDocument();
       expect(screen.getByText((content, element) => {
-        return element.tagName.toLowerCase() === 'p' && content.startsWith('Welcome to Ensign Eats, the ultimate destination for the best burgers in town!')
+          return element.tagName.toLowerCase() === 'p' && content.startsWith('Welcome to Ensign Eats, the ultimate destination for the best burgers in town!');
       })).toBeInTheDocument();
-      expect(screen.getByAltText('Founders')).toBeInTheDocument();
-    });
-  
-    test('renders Our Team section', () => {
+      expect(screen.getByAltText('Founders of Ensign Eats')).toBeInTheDocument();
+  });
+
+  test('renders Our Team section', () => {
       expect(screen.getByText('Our Team')).toBeInTheDocument();
       expect(screen.getByText((content, element) => {
-        return element.tagName.toLowerCase() === 'p' && content.startsWith('Our dedicated team is the heart of Ensign Eats.')
+          return element.tagName.toLowerCase() === 'p' && content.startsWith('Our dedicated team is the heart of Ensign Eats.');
       })).toBeInTheDocument();
       expect(screen.getByText('Congratulations to Monty!')).toBeInTheDocument();
-      expect(screen.getByAltText('Employee of the Month')).toBeInTheDocument();
-    });
-  
-    test('renders video in Our Team section', () => {
+      expect(screen.getByAltText('Employee of the Month: Monty')).toBeInTheDocument();
+  });
+
+  test('renders video in Our Team section', () => {
       const video = screen.getByTestId('kitchen-video');
       expect(video).toBeInTheDocument();
-    });
-  
-    test('renders Awards section', () => {
+  });
+
+  test('renders Awards section', () => {
       expect(screen.getByText('Here you can find the BEST in town!')).toBeInTheDocument();
       expect(screen.getByText((content, element) => {
-        return element.tagName.toLowerCase() === 'p' && content.startsWith('Ensign Eats has won the Best Burger in Salt Lake City award for four years in a row.')
+          return element.tagName.toLowerCase() === 'p' && content.startsWith('Ensign Eats has won the Best Burger in Salt Lake City award for four years in a row.');
       })).toBeInTheDocument();
-      expect(screen.getAllByAltText('Best Burger Award Seal')).toHaveLength(1);
-    });
-  
-    test('renders Best Dessert section', () => {
-      expect(screen.getByText((content, element) => {
-        return element.tagName.toLowerCase() === 'p' && content.startsWith("Along with their burgers, Ensign Eats' Strawberry Shortcake has won the Best Dessert award.")
-      })).toBeInTheDocument();
-      expect(screen.getAllByAltText('Best Burger Award Seal')).toHaveLength(1);
-    });
+      expect(screen.getByAltText('Best Burger Award Seal')).toBeInTheDocument();
   });
+
+  test('renders Best Dessert section', () => {
+      expect(screen.getByText((content, element) => {
+          return element.tagName.toLowerCase() === 'p' && content.startsWith("Along with their burgers, Ensign Eats' Strawberry Shortcake has won the Best Dessert award.");
+      })).toBeInTheDocument();
+      expect(screen.getByAltText('Best Dessert Award Seal')).toBeInTheDocument();
+  });
+});
