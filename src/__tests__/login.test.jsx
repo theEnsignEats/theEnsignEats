71ca847b08170d6888a465d838edb16989107bfc
trujillonefi12 <trujillonefi12@gmail.com>
2024-07-15 14:34:52 -0600

    test impleted

--- src/__tests__/login.test.jsx
+++ src/__tests__/login.test.jsx
@@ -10,7 +10,7 @@ describe("Authentication Component", () => {
 
   test("renders sign-in form", () => {
     render(<Authentication />);
-
+    //CHECK IG ==F THE THINGD ARE RENDERRED? 
     expect(screen.getByText("Sign in to your account")).toBeInTheDocument();
     expect(screen.getByLabelText("Email address")).toBeInTheDocument();
     expect(screen.getByLabelText("Password")).toBeInTheDocument();
@@ -34,7 +34,7 @@ describe("Authentication Component", () => {
     localStorage.setItem("registrationData", JSON.stringify(storedData));
 
     render(<Authentication />);
-
+    // RENDERED AFTER CLICKING AND REGISTERINF
     fireEvent.change(screen.getByLabelText("Email address"), {
       target: { value: "wrong@example.com" },
     });
@@ -46,7 +46,7 @@ describe("Authentication Component", () => {
 
     expect(screen.getByText("Invalid email or password.")).toBeInTheDocument();
   });
-
+  // CHECKING IF THE ACCOUNT EXISTS
   test("shows success message for valid credentials", () => {
     const storedData = { email: "test@example.com", password: "password123" };
     localStorage.setItem("registrationData", JSON.stringify(storedData));
@@ -64,7 +64,7 @@ describe("Authentication Component", () => {
 
     expect(screen.getByText("Login Successful!")).toBeInTheDocument();
   });
-
+    // IF THE ACCOUNT DOES NT EXISTS
   test("shows error if no account is found", () => {
     render(<Authentication />);
 
--- src/login/page.jsx
+++ src/login/page.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 
-export default function Example() {
+export default function Authentication() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
