8c1a656080649ac7040fa110b8bb6a4e4bc7fa2a
Pedro Perillo <pedrofperillo@gmail.com>
2024-07-15 17:09:22 -0600

    added button, will continue to make changes

--- src/components/floatingCartButton.jsx
+++ src/components/floatingCartButton.jsx
@@ -4,7 +4,7 @@ export default function FloatingCartButton() {
     return (
         <div className="fixed bottom-2 right-2">
             <button type="button"
-                    className="rounded-full bg-indigo-600 p-4 m-10 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="rounded-full bg-green-800 p-4 m-10 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
                     aria-label="Open Cart">
                 <img src="cart.png" alt="Open Cart" width={30} height={30}/>
             </button>
